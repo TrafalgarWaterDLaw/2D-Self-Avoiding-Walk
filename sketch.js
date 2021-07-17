bb42e31bb6e012d016ea1dd35e94ec130446990f
Richard.G <77013205+TrafalgarWaterDLaw@users.noreply.github.com>
2021-07-17 18:35:39 +0200

    committed

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+let Alloptions = [
+	{ dx: 1, dy: 0 }, //Right
+	{dx: -1, dy: 0}, //Left
+	{dx: 0, dy: 1}, //Down
+	{dx: 0, dy: -1} //Up
+]
+
 let x
 let y
 let grid
@@ -21,25 +28,51 @@ function setup() {
 	y = rows / 2
 	background(51)
 	grid = make2DArray(cols, rows)
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			grid[i][j] =false
+		} 
+	}				
+	grid[x][y] = true
+}
+
+function isValid(i, j) {
+	if (i < 0 || i >= cols || j < 0 || j >= rows) {
+		return false
+	}
+	return !grid[i][j]
 }
 
 function draw() {
-	stroke(255, 100)
+	stroke(255)
 	strokeWeight(spacing * 0.5)
 	point(x * spacing, y * spacing)
-	const r = floor(random(4))
-	switch (r) {
-		case 0:
-			x = x + 1
-			break
-		case 1:
-			x = x - 1
-			break
-		case 2:
-			y = y + 1
-			break
-		case 3:
-			y = y - 1
-			break
+
+	let options = []
+	for (let option of Alloptions) {
+		let newX = x + option.dx
+		let newY = y + option.dy
+		if (isValid(newX, newY)) {
+			options.push(option)
+		 }
+	}
+
+	if (options.length > 0) {
+		let step = random(options)
+
+		strokeWeight(1)
+		stroke(255)
+		beginShape()
+		vertex(x * spacing, y * spacing)
+		x += step.dx
+		y += step.dy
+		vertex(x * spacing, y * spacing)
+		endShape()
+
+		grid[x][y] = true
+		
+	} else {
+		console.log("Stuck")
+		noLoop()
 	}
 }
